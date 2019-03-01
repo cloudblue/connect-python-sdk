efeebdd07a7815d2379c4a71e9ace418207347e4
Javier San Juan Cervera <Javier.SanJuanCervera@ingrammicro.com>
2019-03-01 11:43:46 +0100

    Added copyright notice on all Python files

--- connect/__init__.py
+++ connect/__init__.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+"""
+This file is part of the Ingram Micro Cloud Blue Connect SDK.
+Copyright (c) 2019 Ingram Micro. All Rights Reserved.
+"""
+
 from .resource import FulfillmentAutomation
 
 name = 'connect'
--- connect/models/asset.py
+++ connect/models/asset.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+"""
+This file is part of the Ingram Micro Cloud Blue Connect SDK.
+Copyright (c) 2019 Ingram Micro. All Rights Reserved.
+"""
+
 from marshmallow import fields, post_load
 
 from .base import BaseModel, BaseSchema
