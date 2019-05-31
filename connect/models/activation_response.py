12f88bab525c818ec51c001745506e354a7cdd92
Javier San Juan Cervera <Javier.SanJuanCervera@ingrammicro.com>
2019-05-31 12:11:47 +0200

    Fixed documentation of FulfillmentAutomation and TierConfigAutomation, and added documentation for ActivationTemplateResponse and ActivationTileResponse.

--- connect/models/activation_response.py
+++ connect/models/activation_response.py
@@ -7,10 +7,18 @@ import json
 
 
 class ActivationTileResponse(object):
+    """ An instance of this class might the returned by the overriden ``process_request`` method
+    of your :py:class:`connect.resources.FulfillmentAutomation` or
+    :py:class:`connect.resources.TierConfigAutomation` subclass to approve the request being
+    processed, showing a tile with the specified contents.
+
+    :param str markdown: Contents of the tile to be shown in the Vendor Portal, in Markdown format.
+    :rtype: None
+    """
+
     tile = 'Activation succeeded'  # type: str
 
     def __init__(self, markdown=''):
-        # type: (str) -> None
         try:
             self.tile = json.loads(markdown)
         except ValueError:
@@ -18,8 +26,17 @@ class ActivationTileResponse(object):
 
 
 class ActivationTemplateResponse(object):
+    """ An instance of this class might the returned by the overriden ``process_request`` method
+    of your :py:class:`connect.resources.FulfillmentAutomation` or
+    :py:class:`connect.resources.TierConfigAutomation` subclass to approve the request being
+    processed, showing a tile with the specified template id.
+
+    :param str template_id: Id of the template od the tile to be shown in the Vendor Portal.
+        The template must have been defined in the Vendor Portal.
+    :rtype: None
+    """
+
     template_id = None  # type: str
 
     def __init__(self, template_id):
-        # type: (str) -> None
         self.template_id = template_id
