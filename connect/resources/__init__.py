4dcf02b33d30f5700130609be765657d9d940239
Martin Constante <martin.constante@ingrammicro.com>
2020-04-06 09:58:47 +0200

    Change copyright text

--- connect/resources/__init__.py
+++ connect/resources/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # This file is part of the Ingram Micro Cloud Blue Connect SDK.
-# Copyright (c) 2020 Ingram Micro. All Rights Reserved.
+# Copyright (c) 2019-2020 Ingram Micro. All Rights Reserved.
 
 from .directory import Directory
 from .fulfillment_automation import FulfillmentAutomation
--- tests/test_tier_account_request.py
+++ tests/test_tier_account_request.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # This file is part of the Ingram Micro Cloud Blue Connect SDK.
-# Copyright (c) 2020 Ingram Micro. All Rights Reserved.
+# Copyright (c) 2019-2020 Ingram Micro. All Rights Reserved.
 
 import os
 import unittest
